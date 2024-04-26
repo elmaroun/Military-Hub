33fa8a3968e91c535002ee0e57a0b510ecbdd05b
rajae <elabida.rajae@etu.uae.ac.ma>
2024-04-26 13:04:26 +0100

    YGA-22 <creation du footer>

---
+++ app.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="header_component/header_component.css">
+    <link rel="stylesheet" href="footer_component/footer_component.css">
+</head>
+<body>
+    <?php include 'header_component\header_component.php'; ?>
+
+
+    
+</body>
+</html>
---
+++ header_component/header_component.php
@@ -0,0 +1,6 @@
+<header>
+    <div class="header">
+        <h1 class="animation">ArmyHub</h1>
+        <h2>Explorez les armées du monde entier</h2>
+    </div>
+</header>
