e524da3078321db3642073e73778777e8c50d22a
Ilias Maroun <111662661+elmaroun@users.noreply.github.com>
2024-05-09 11:31:40 +0100

    Update footer_component.php

--- footer_component/footer_component.php
+++ footer_component/footer_component.php
@@ -1,10 +1,9 @@
+<link rel="stylesheet" href="footer_component.css">
+
 <footer>
 <div class="footer">
     <div >
-        <a href="aboutus.html" style="color: white"><h3 class="title" >À propos de nous</h3></a>
-    </div>
-    <div >
-        <h3 >&copy; ArmyHub</h3>
+        <a href="aboutus.html" style="color: white;"> <h4 style="margin: 3px 0 6px;"> À propos de nous ㅤㅤㅤㅤ &copy; ArmyHub </h4> </a>
     </div>
 </div>
 </footer>
